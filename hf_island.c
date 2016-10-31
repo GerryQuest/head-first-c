5fb61e226eafb7e18af64565cb1a8e89db30a03d
Eissek <sane.gez@gmail.com>
2016-10-31 18:37:26 +0000

    added linked list to struc

--- hf_island.c
+++ hf_island.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct island {
   char *name;
   char *opens;
   char *closes;
+  struct island *next;
 } island;
 
 void printIslandData (island *island) {
   printf("...\n%s\n%s\n%s\nCompleted.\n",
 	 island->name, island->opens, island->closes);
+  if (island->next != NULL)
+    printf("\nNext: %s\n", island->next->name);
 }
 
 int main () {
   puts("Hi...");
-  island lost = {"Lost Island", "9:00", "18:00"};
-  printIslandData (&lost);
+  island craggy = {"Craggy island", "08:00", "18:00", NULL};
+  island lost = {"Lost Island", "9:00", "18:00", NULL};
+  craggy.next = &lost;
+  
+  printIslandData (&craggy);
   return 0;
 }
